3c8044f560ff10dcacbec33554a00415349f3446
Kielx <kiel66@gmail.com>
2021-09-05 00:13:29 +0200

    Add images display with metadata fetching, sorting of images, display breakpoints and styling with overlay name

--- components/MasonryContainer.js
+++ components/MasonryContainer.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Masonry from "react-masonry-css";
-import { storageRef, listAll, getDownloadURL } from "../firebase/firebase";
+import {
+  storageRef,
+  listAll,
+  getDownloadURL,
+  getMetadata,
+} from "../firebase/firebase";
 import Image from "next/image";
 
 const MasonryContainer = () => {
@@ -16,21 +21,55 @@ const MasonryContainer = () => {
       return Promise.all(urlPromises);
     };
 
+    const fetchMetadata = async () => {
+      let result = await listAll(storageRef);
+      let metadataPromises = result.items.map((imageRef) =>
+        getMetadata(imageRef)
+      );
+      return Promise.all(metadataPromises);
+    };
+
     const loadImages = async () => {
       const urls = await fetchImages();
-      setFiles(urls);
+      const metadata = await fetchMetadata();
+      //for in each url, get the metadata and add it to the url
+      let images = urls.map((url, index) => {
+        return {
+          url: url,
+          metadata: metadata[index],
+        };
+      });
+      images.sort(
+        (a, b) => new Date(b.metadata.updated) - new Date(a.metadata.updated)
+      );
+      setFiles(images);
     };
     loadImages();
   }, []);
+  console.log(files);
+
+  //Masonry breakpoint columns object for different screen sizes
+  const breakpointColumnsObj = {
+    default: 3,
+    2200: 5,
+    1600: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
 
   return (
     <Masonry
-      breakpointCols={3}
+      breakpointCols={breakpointColumnsObj}
       className="my-masonry-grid pt-10"
       columnClassName="my-masonry-grid_column"
     >
       {files?.map((file) => (
-        <img key={file} src={file} alt="masonry" layout="fill" />
+        <div key={file.url} className="imageContainer">
+          {/* eslint-disable-next-line @next/next/no-img-element*/}
+          <img src={file.url} alt="My unsplash image" className="image" />
+          <div className="overlay">{file.metadata.name}</div>
+        </div>
       ))}
     </Masonry>
   );
