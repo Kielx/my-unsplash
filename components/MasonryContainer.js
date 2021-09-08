dfb712e8b68a3891b742d2d15bec48827bf88c4c
Kielx <kiel66@gmail.com>
2021-09-08 21:40:23 +0200

    Check image components width and height with custom metadata for better rendering with Next Image component

--- components/MasonryContainer.js
+++ components/MasonryContainer.js
@@ -2,15 +2,100 @@ import React, { useState, useEffect } from "react";
 import Masonry from "react-masonry-css";
 import {
   storageRef,
+  getStorage,
   listAll,
   getDownloadURL,
   getMetadata,
+  uploadBytesResumable,
+  ref,
 } from "../firebase/firebase";
-import Image from "next/image";
+import NextImage from "next/image";
 
 const MasonryContainer = () => {
   // Get all the images from Storage
   const [files, setFiles] = useState();
+  //selected file
+  const [selectedFile, setSelectedFile] = useState();
+
+  const upload = async (file) => {
+    // Create the file metadata
+    /** @type {any} */
+    const metadata = {
+      contentType: "image/*",
+      customMetadata: {
+        width: "0",
+        height: "0",
+      },
+    };
+
+    //check file width and height
+    const myImage = new Image();
+    myImage.src = window.URL.createObjectURL(file);
+    myImage.onload = function () {
+      const width = myImage.naturalWidth;
+      const height = myImage.naturalHeight;
+      metadata.customMetadata.width = width;
+      metadata.customMetadata.height = height;
+      window.URL.revokeObjectURL(myImage.src);
+      console.log(width, height);
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      const storage = getStorage();
+      const storageRef = ref(storage, `/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          //dispatch(setLoading(true));
+          //dispatch(
+          //setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+          //);
+          //console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              //console.log("Upload is paused");
+              break;
+            case "running":
+              //console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          //console.log(error);
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+            default:
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          //dispatch(setLoading(false));
+          //dispatch(setLoaded(true));
+          //getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          // dispatch(setDownloadURL(downloadURL))
+          //);
+        }
+      );
+    };
+  };
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -65,24 +150,47 @@ const MasonryContainer = () => {
   };
 
   return (
-    <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className="my-masonry-grid pt-10"
-      columnClassName="my-masonry-grid_column"
-    >
-      {files?.map((file) => (
-        <div key={file.url} className="imageContainer">
-          {/* eslint-disable-next-line @next/next/no-img-element*/}
-          <Image
-            src={file.url}
-            alt="My unsplash image"
-            width="400"
-            height="400"
-          />
-          <div className="overlay">{file.metadata.name}</div>
-        </div>
-      ))}
-    </Masonry>
+    <>
+      <form>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => setSelectedFile(e.target.files[0])}
+        ></input>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            upload(selectedFile);
+          }}
+        >
+          Upload file
+        </button>
+      </form>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid pt-10"
+        columnClassName="my-masonry-grid_column"
+      >
+        {files?.map(
+          (file) => (
+            console.log(file),
+            (
+              <div key={file.url} className="imageContainer">
+                {/* eslint-disable-next-line @next/next/no-img-element*/}
+                <NextImage
+                  src={file.url}
+                  alt="My unsplash image"
+                  width={`${file.metadata?.customMetadata?.width || "400"}`}
+                  height={`${file.metadata?.customMetadata?.height || "400"}`}
+                />
+                <div className="overlay">{file.metadata.name}</div>
+              </div>
+            )
+          )
+        )}
+      </Masonry>
+    </>
   );
 };
 
